d42ba6cc8b41d1a55ec31b49d48b188172db432e
sujanos <wegonar@outlook.com>
2025-11-01 19:40:32 +0100

    feat: add jump to item functionallity

--- src/app/components/list/list.ts
+++ src/app/components/list/list.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import { FormsModule } from '@angular/forms';
+import { ScrollingModule, CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ScrollingModule],
+  imports: [CommonModule, FormsModule, ScrollingModule],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
 export class List {
+  @ViewChild('scrollViewport') scrollViewport!: CdkVirtualScrollViewport;
+  
   list = Array.from({ length: 1_000_000 }, (_, i) => ({
     id: i + 1,
     content: `Item ${i + 1}`,
   }));
 
   itemHeight = 54;
+
+  jumpToItem(itemNumber: number): void {
+    if (itemNumber >= 1 && itemNumber <= this.list.length) {
+      const index = itemNumber - 1;
+      this.scrollViewport.scrollToIndex(index, 'smooth');
+    }
+  }
 }
