6a97c20cf496b580beed2851c6d2d174ee4b9b85
sujanos <wegonar@outlook.com>
2025-11-01 19:22:37 +0100

    feat: implement virtual scrolling for 1 million items

--- src/app/components/list/list.ts
+++ src/app/components/list/list.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollingModule],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
 export class List {
-  list = Array.from({ length: 50 }, (_, i) => ({ id: i + 1, content: `Item ${i + 1}` }));
+  list = Array.from({ length: 1_000_000 }, (_, i) => ({
+    id: i + 1,
+    content: `Item ${i + 1}`,
+  }));
+
+  itemHeight = 54;
 }
